aad3428405a81226829bf7e0f8e7bb81d6f32395
Rupatarini <tarini.rupeswari@gmail.com>
2024-07-24 23:46:52 +0530

    Create 2d.py

---
+++ 2d.py
@@ -0,0 +1,6 @@
+import numpy as np
+matrix = np.array([[1, 2, 3],
+                   [4, 5, 6],
+                   [7, 8, 9]])
+last_column = matrix[:, -1]
+print("The final matrix is :",last_column)
